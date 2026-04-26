56d5703c904d04b338c468dbd52d195c706e6dd3
salmaax <salmaakhallaf@gmail.com>
2026-04-26 23:40:04 +0300

    classification + final pipeline

--- main.py
+++
@@ -1,1 +0,0 @@
-# Multi-Class Brain Tumor Classification from MRI Images
--- pipeline.py
+++
@@ -1,4 +0,0 @@
-# Multi-Class Brain Tumor Classification from MRI Images
-
-
-# image → preprocessing → segmentation → feature extraction → classification → output label
